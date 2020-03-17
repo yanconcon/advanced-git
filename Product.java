365c13a0413042c6f2c9517d0256f4f78bd4dae8
yanwencong <1029878802@qq.com>
2020-03-17 22:14:54 +0800

    revert to 8.0

--- Product.java
+++ Product.java
@@ -1,6 +1,6 @@
 public class Product {
     public static void main(String[] args) {
-        System.out.println("I'm version 10.0!");
-        System.out.println("A bad feature!");
+        System.out.println("I'm version 8.0!");
+        System.out.println("I have a bug!");
     }
 } 
